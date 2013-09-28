aeae2690ae524b703e9e0849a78d190204f90abb
Julia Scherbina <julia.shcherbina@djangostars.com>
2013-09-28 22:16:26 +0300

    Add form for create task

--- design/js/brainPanic.js
+++ design/js/brainPanic.js
@@ -1,7 +1,22 @@
 $(function(){
   
-  (function(){
-    
+  // SHOW/HIDE POPUP
+  (function() {
+    var popup = $('.js-popup-task-add'),
+        showPopup = $('.js-task-add'),
+        close = popup.find('.popup-close');
+        console.log(popup);
+        
+    showPopup.click('live', function(e) {
+      popup.hide();       
+      popup.fadeIn('slow');
+      e.preventDefault();    
+    });
+
+    close.click('live', function(e) {
+      popup.hide();   
+      e.preventDefault();    
+    });
   })();
   
 });
