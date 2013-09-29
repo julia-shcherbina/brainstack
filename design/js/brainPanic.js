f2f8cdb2f20e7b69d37cc1a5fcbdb047f770c9ee
Julia Scherbina <julia.shcherbina@djangostars.com>
2013-09-29 10:54:53 +0300

    Add checkbox for new task, show/hide description, change  status

--- design/js/brainPanic.js
+++ design/js/brainPanic.js
@@ -1,21 +1,69 @@
 $(function(){
   
-  // SHOW/HIDE POPUP
+  // SHOW/HIDE POPUP CREATE TASK
   (function() {
     var popup = $('.js-popup-task-add'),
-        showPopup = $('.js-task-add'),
-        close = popup.find('.popup-close');
-        console.log(popup);
+        showPopup = $('.js-task-add');
+        //close = popup.find('.popup-close');
         
     showPopup.click('live', function(e) {
       popup.hide();       
-      popup.fadeIn('slow');
+      //popup.fadeIn('slow');
+      popup.animate({ height: "show"}, 500, function() {});
+      
       e.preventDefault();    
     });
 
-    close.click('live', function(e) {
+    /*close.click('live', function(e) {
       popup.hide();   
       e.preventDefault();    
+    });*/
+  })();
+  
+  //CHECKBOX
+  (function(){
+    var checkbox = $('.js-checkbox');
+    
+    checkbox.each(function(index,value){
+      var input = $(this).children('[type=checkbox]');
+      input.hide();
+      var widget = $('<a class="checked" href="javascript:;"></a>' +'<a class="unchecked" href="javascript:;"></a>');
+      widget.insertBefore($(input)).hide();
+      
+      var checked = widget.closest('.checked'),
+      unchecked = widget.closest('.unchecked');
+      if (input.attr('checked') === 'checked') {
+        $(checked).show();
+      } else {
+        $(unchecked).show();
+      }
+      
+      $(checked).on('click',function(e){
+        $(this).hide();
+        $(unchecked).show();
+        $(input).attr('checked', null);
+        $(input).change();
+        e.preventDefault();
+      });
+     
+     $(unchecked).on('click',function(e){
+       $(this).hide();
+       $(checked).show();
+       $(input).attr('checked', 'checked');
+       $(input).change();
+       e.preventDefault();
+     });
+    });
+  })(); 
+  
+  // SHOW/HIDE DESC
+  (function() {
+    var taskDesc = $('.js-task-desc'),
+        toggleDesc= $('.js-toggle-task-desc');
+    toggleDesc.click(function(e){
+      //$(taskDesc).hide();
+      $(this).closest('.task-item').find('.js-task-desc').animate({ height: "toggle"}, 500, function() {});
+      e.preventDefault(); 
     });
   })();
   
